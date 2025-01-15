276e99ce2342236285d08f0ba3287319b740c186
rachelmalbert <albert.rachelm@gmail.com>
2025-01-15 15:22:47 -0500

    update baseurl

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 const api = (token) => {
   // const baseUrl = "http://localhost:8000";
   // const baseUrl = "https://zzsewbxcaahebc2xpdrp7r3xxy0gfrzr.lambda-url.us-east-2.on.aws";
-  const baseUrl = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+  const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
   const headers = {
     "Content-Type": "application/json",
